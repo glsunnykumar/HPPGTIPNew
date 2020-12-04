67b693ac35d713104f2a2a8d3281dd535f9bcfff
glsunnykumar <glsunny.kumar@gmail.com>
2020-12-04 11:54:27 +0530

    sdsdsd

--- backend/app.js
+++ backend/app.js
@@ -24,11 +24,14 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader(
         "Access-Control-Allow-Headers",
-        "Origin,X-Requested-With,Content-Type,Accept"
+        "Origin,X-Requested-With,Content-Type,Accept,Authorization"
     );
     res.setHeader(
         "Access-Control-Allow-Methods",
         "GET,POST,PATCH,DELETE,PUT,OPTIONS"
+    );
+    res.setHeader("Content-Type",
+    "application/json"
     );
     next();
 });
